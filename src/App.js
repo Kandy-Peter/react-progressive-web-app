f6aff41f85071d93ce1b8ef350eb1550e74ccb4b
kandy-peter <kandypeter03@gmail.com>
2022-03-27 23:05:06 +0200

    add-routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './styles/style.css';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+       <Route path='/' exact element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import NetflixLogo from '../icons/NetflixLogo';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
     <div id="logo" className="logo">
-        <NetflixLogo />
+        <Link to='/'>
+            <NetflixLogo />
+        </Link>
     </div>
 )
 
