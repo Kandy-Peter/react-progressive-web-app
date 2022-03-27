6464e88266fb2be0131b50c8a1d7c53e30652a7c
kandy-peter <kandypeter03@gmail.com>
2022-03-27 22:17:33 +0200

    use route and intsall svg npm and its uses

---
+++ src/components/layout/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NetflixLogo from '../icons/NetflixLogo';
+
+const Header = () => (
+    <NetflixLogo />
+)
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/layout/index';
+
+const Home = () => (
+    <Layout>
+        This the layout child
+    </Layout>
+)
+
+export default Home;
