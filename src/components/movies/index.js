89906d7bef2b011eab787185ebc27c34406edfb7
kandy-peter <kandypeter03@gmail.com>
2022-03-28 21:25:22 +0200

    fethc data and dipslay them to the screen

--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Movie from './movie';
 
 const Movies = () => {
     const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
-  console.log(process.env.REACT_APP_MOVIE_API_KEY)
 
   useEffect(() => {
     setLoading(true);
 
     axios.get(`https://api.themoviedb.org/3/discover/tv`, {
       params: {
-        sort_by: 'popularity.desc',
         api_key: process.env.REACT_APP_MOVIE_API_KEY,
       }
     })
@@ -27,8 +26,7 @@ const Movies = () => {
 
   return (
       <div className="flex flex-wrap -mb-4">
-        My movie
-        {/* {!loading && movies?.length ? movies.map( (movie, index) =>  <Movie key={movie?.id ?? index} movie={movie}/>) : <h2>Loading...</h2>} */}
+        {!loading && movies?.length ? movies.map( (movie, index) =>  <Movie key={movie?.id ?? index} movie={movie}/>) : <h2>Loading...</h2>}
       </div>
     )
   }
